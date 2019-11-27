c01f6cb32f7893b277f5f92111ec07c987397714
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-27 15:24:47 +0530

    added patterns learned

--- 4. Greedy Algorithms.py
+++ 4. Greedy Algorithms.py
@@ -19,3 +19,13 @@ def get_max_profit(stock_prices):
         min_price = min(price, min_price)
     
     return max_profit
+
+# PATTERN LEARNED
+'''
+Greedy algorithms are good because they are fast. They usually take one pass through the input.
+
+How do you know if a problem will lend itself to a greedy approach? Best bet is to try it out and see if it works. Trying out a greedy approach should be one of the 
+
+To try it on a new problem, start by asking yourself:
+"Suppose we could come up with the answer in one pass through the input, by simply updating the 'best answer so far' as we went. What "additional values" would we need to keep updated as we looked at each item in our input, in order to be able to update the "best answer so far" in constant time?"
+'''
