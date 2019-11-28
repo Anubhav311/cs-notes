ed4b97f4827144145a838ed6fb356278d376e947
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-28 18:25:25 +0530

    further improved the solution

--- 5. Sorting, Searching, Logarithms.py
+++ 5. Sorting, Searching, Logarithms.py
@@ -51,8 +51,34 @@ def find_repeat(numbers):
     #         if numbers[i] == numbers[j]:
     #             return numbers[i]
 
-    # O(n log n) time and O(1) space. But here we are modifying the input.
-    numbers.sort()
-    for i in range(len(numbers)):
-        if numbers[i] == numbers[i+1]:
-            return numbers[i]
+    # # O(n log n) time and O(1) space. But here we are modifying the input.
+    # numbers.sort()
+    # for i in range(len(numbers)):
+    #     if numbers[i] == numbers[i+1]:
+    #         return numbers[i]
+
+    #O(n log n) time and O(1) space. Without modifying the input.
+    floor = 1
+    ceiling = len(numbers) - 1
+
+    while floor < ceiling:
+        midpoint = floor + ((ceiling - floor) / 2)
+        lower_range_floor, lower_range_ceiling = floor, midpoint
+        upper_range_floor, upper_range_ceiling = midpoint+1, ceiling
+
+        items_in_lower_range = 0
+        for item in numbers:
+            if item >= lower_range_floor and item <= lower_range_ceiling:
+                items_in_lower_range += 1
+
+        distinct_possible_integers_in_lower_range = (
+            lower_range_ceiling
+            - lower_range_floor
+            + 1
+        )
+        if items_in_lower_range > distinct_possible_integers_in_lower_range:
+            floor, ceiling = lower_range_floor, lower_range_ceiling
+        else:
+            floor, ceiling = upper_range_floor, upper_range_ceiling
+
+    return floor
