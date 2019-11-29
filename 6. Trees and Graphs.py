6e7275d243be2cb2ffe47ea907d7d5d49db81d24
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-29 15:52:52 +0530

    solution to graph coloring

--- 6. Trees and Graphs.py
+++ 6. Trees and Graphs.py
@@ -136,6 +136,27 @@ Here we used a "simplify, solve, and adapt" strategy.
 
 
 # GRAPH COLORING
+def color_graph(graph, colors):
+    for node in graph:
+        if node in node.neighbors:
+            raise Exception('Legal coloring impossible for node with loop: %s' %
+                            node.label)
+
+        # Get the node's neighbors' colors, as a set so we
+        # can check if a color is illegal in constant time
+        illegal_colors = set([
+            neighbor.color
+            for neighbor in node.neighbors
+            if neighbor.color
+        ])
+
+        # Assign the first legal color
+        for color in colors:
+            if color not in illegal_colors:
+                node.color = color
+                break
+
+
 # proof by induction that D + 1 is always enough for legal coloring
 '''
 In general, an inductive proof uses 2 steps to prove a claim is true for all (usually positive) integers:
