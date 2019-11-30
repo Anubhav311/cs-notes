27b25e928828c1a50b1c1d2a28a052933a1c9588
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-30 18:29:50 +0530

    added one more pattern

--- 7. Dynamic programming and recursion.py
+++ 7. Dynamic programming and recursion.py
@@ -65,4 +65,17 @@ def fib(n):
 # PATTERN LEARNED
 '''
 Iterative solutions can be more efficient than recursive solution in terms of memory.
+
+It's usually best to avoid recursion where dataset is big because it can cause "stack overflow".
+'''
+
+
+# MAKING CHANGE
+
+
+# PATTERN LEARNED
+'''
+Dynamic programming is kind of like the next step up from greedy. You're taking that idea of "keeping track of what we need in order to update the best answer so far," and applying it to situations where the new best answer so far might not just have to do with the previous answer, but some other earlier answer as well.
+
+In greedy algorithms, we were keeping track of our prev answers and updating the current solution. Now the thing we need to keep track of is all our previous answers, which we're keeping in a list.
 '''
